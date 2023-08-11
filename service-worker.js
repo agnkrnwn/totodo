c7d55fd36943f6c269ab74e875f41eb3475ab6af
agungk <AGUNG9367@GMAIL.COM>
2023-08-11 23:36:41 +0700

    Update service-worker.js
    off

--- service-worker.js
+++ service-worker.js
@@ -12,5 +12,10 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => response || fetch(event.request))
+      .catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('/');
+        }
+      })
   );
 });
